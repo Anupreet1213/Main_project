5ad28124e8e8e35cff7e0655a535712cc7a657e4
AmanApT <amanasstudent@gmail.com>
2023-04-21 21:22:07 +0530

    Card Theek

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,11 @@
-import React,{useState,useEffect} from "react";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 const Hero = () => {
-
   const progressPercentage = 80;
+
+  const [payButton, setPayButton] = useState(0);
   const [countdown, setCountdown] = useState({
     days: 0,
     hours: 0,
@@ -49,11 +50,20 @@ const Hero = () => {
             Simplify Smart Contracts: What They Are and How They Work
           </p>
           <div className="heroSectionLeftDescription2 word-rotator">
-            <span className="word-1">Deploying a Smart Contract on the Ethereum Blockchain: A Step-by-Step Guide</span>
-            <span className="word-2">An expert in all things crypto and blockchain, at your service!</span>
-            <span className="word-3">Running a Bitcoin Wallet Locally: A Quick How-To Guide</span>
-            <span className="word-4">Stay Up-to-Date on Crypto News with This Summary of the Latest Developments</span>
-       
+            <span className="word-1">
+              Deploying a Smart Contract on the Ethereum Blockchain: A
+              Step-by-Step Guide
+            </span>
+            <span className="word-2">
+              An expert in all things crypto and blockchain, at your service!
+            </span>
+            <span className="word-3">
+              Running a Bitcoin Wallet Locally: A Quick How-To Guide
+            </span>
+            <span className="word-4">
+              Stay Up-to-Date on Crypto News with This Summary of the Latest
+              Developments
+            </span>
           </div>
           <div className="heroSectionButtons">
             <div> Book your Seat</div>
@@ -61,57 +71,65 @@ const Hero = () => {
           </div>
         </div>
 
-
-
         <div className="heroSectionRight">
           <div className="heroSectionRightTop">
-          <p className="heroSectionCounter">
+            <p className="heroSectionCounter">
               {countdown.days}D : {countdown.hours}H : {countdown.minutes}M :{" "}
               {countdown.seconds}S
             </p>
+
             <div className="progressSection">
-              <div className="progressBar" style={{ width: `${progressPercentage}%` }}>
-                {/* <div className="progressLabel">{`${progressPercentage}%`}</div> */}
-                <div className="progressLabel">Until Next Price $0.007</div>
+              {/* <div className="progressBar" style={{ width: `${progressPercentage}%` }}>
+              </div> */}
+              <div
+                style={{ width: `${progressPercentage}%` }}
+                className="progressLabel"
+              >
+                Until Next Price $0.007
               </div>
             </div>
+
             <p className="heroSectionRightCenterText3">
-            USDT raised: $2,059,925 / $2,450,000
+              USDT raised: $2,059,925 / $2,450,000
             </p>
             <p className="heroSectionRightCenterText1">LISTING PRICE: $0.01</p>
           </div>
-        
+
           <div className="heroSectionRightCenter">
-            <p className="heroSectionRightCenterText2">----1 ECOTERRA = $0.00625----</p>
-        
-            <p className="heroSectionRightCenterText5">
-              USDT Raised — $0 / $2,000,000
-            </p>
+            <div id="container">
+              <div class="line"></div>
+              <div class="content">1 ECOTERRA = $0.00625</div>
+              <div class="line"></div>
+            </div>
+              <div className="ThreeButtonsWrapper">
+                  <div className={payButton ===0 ? "activeButton" : "buttonsKeDiv"} onClick={()=>setPayButton(0)}>ETH</div>
+                  <div className={payButton ===1 ? "activeButton" : "buttonsKeDiv2"} onClick={()=>setPayButton(1)}>USDT</div>
+                  <div className={payButton ===2 ? "activeButton" : "buttonsKeDiv3"} onClick={()=>setPayButton(2)}>CARD</div>
+              </div>
             <TextField
-          id="filled-multiline-flexible"
-          label="Amount in USD you pay
-          "
-          multiline
-          maxRows={4}
-          variant="filled"
-        />
+              className="heroSectionTextFields"
+              id="filled-multiline-flexible"
+              label={`Amount in ${payButton===0 ? "ETH" : payButton===1 ? "USD" : "CARD"} you pay`}
+              multiline
+              maxRows={4}
+              variant="filled"
+            />
+            <div style={{height:"10px",width:"10px",backgroundColor:"white"}}> </div>
             <TextField
-          id="filled-multiline-flexible"
-          label="Amount in ECOTERRA you receive"
-          multiline
-          maxRows={4}
-          variant="filled"
-          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-         
-        />
+              className="heroSectionTextFields heroSectionTextFields2"
+              id="filled-multiline-flexible"
+              label="Amount in ECOTERRA you receive"
+              multiline
+              maxRows={4}
+              variant="filled"
+              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+            />
             <div className="heroSectionRightPurpleButtons">
-              <div>BUY WITH WALLET</div>
-            
+              <div>CONNECT WALLET</div>
             </div>
-            <div className="heroSectionRightWhiteButtons">
-              <div>HOW TO BUY</div>
-              <div>NEW TO CRYPTO</div>
-            </div>
+        
+              <div style={{padding:"4%"}}>HOW TO BUY?</div>
+           
           </div>
         </div>
       </div>
