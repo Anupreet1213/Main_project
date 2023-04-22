8183b0414b699cc36878341b83c0193e207da276
AmanApT <amanasstudent@gmail.com>
2023-04-22 14:55:08 +0530

    Responsiveness Fixed 1

--- src/components/StagesSection/StagesSection.js
+++ src/components/StagesSection/StagesSection.js
@@ -12,27 +12,27 @@ const StagesSection = () => {
         </div>
         <hr />
         <div className="stageTwo">
-          <div className="stageTwoButton">Stage 1</div>
-          <p>Total Tokens 1 Billion</p>
-          <p>Per Token Price = $0.002</p>
+          <div className="stageTwoButton">Stage 2</div>
+          <p>Total Tokens 500 Million</p>
+          <p>Per Token Price = $0.0026</p>
         </div>
         <hr />
         <div className="stageThree">
-          <div className="stageThreeButton">Stage 1</div>
-          <p>Total Tokens 1 Billion</p>
-          <p>Per Token Price = $0.002</p>
+          <div className="stageThreeButton">Stage 3</div>
+          <p>Total Tokens 500 Million</p>
+          <p>Per Token Price = $0.0033</p>
         </div>
         <hr />
         <div className="stageFour">
-          <div className="stageFourButton">Stage 1</div>
-          <p>Total Tokens 1 Billion</p>
-          <p>Per Token Price = $0.002</p>
+          <div className="stageFourButton">Stage 4</div>
+          <p>Total Tokens 500 Million</p>
+          <p>Per Token Price = $0.0042</p>
         </div>
         <hr />
         <div className="stageFive">
           <div className="stageFiveButton">Stage 5</div>
-          <p>Total Tokens 1 Billion</p>
-          <p>Per Token Price = $0.002</p>
+          <p>Total Tokens 500 Million</p>
+          <p>Per Token Price = $0.0054</p>
         </div>
       </div>
     </div>
