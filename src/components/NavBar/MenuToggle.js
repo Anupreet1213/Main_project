cde8ff655873afe142e0d54c746fa74149be93ec
Anupreet1213 <anupreet.2003@gmail.com>
2023-04-23 18:47:48 +0530

    Fixed navbar

--- src/components/NavBar/MenuToggle.js
+++ src/components/NavBar/MenuToggle.js
@@ -22,30 +22,32 @@ const Path = (props) => (
 
 const transition = { duration: 0.3 };
 
-export function MenuToggle({ toggle, isOpen, isLightTheme }) {
+export function MenuToggle({ toggle, isOpen }) {
+  const isLightTheme = false;
+
   return (
     <Button onClick={toggle}>
       <svg width="23" height="23" viewBox="0 -4 23 23">
         <Path
           animate={isOpen ? "open" : "closed"}
-          initial={false}
+          initial="closed"
           variants={{
             closed: {
               d: "M 2 2.5 L 20 2.5",
-              stroke: "hsl(100%, 100%, 100%)",
+              stroke: isLightTheme ? "hsl(0, 0%, 0%)" : "hsl(100%, 100%, 100%)",
             },
             open: {
               d: "M 3 16.5 L 17 2.5",
-              stroke: "hsl(100%, 100%, 100%)",
+              stroke: isLightTheme ? "hsl(0, 0%, 0%)" : "hsl(100%, 100%, 100%)",
             },
           }}
           transition={transition}
         />
         <Path
           d="M 2 9.423 L 20 9.423"
-          stroke={"hsl(0, 0%, 100%)"}
+          stroke={isLightTheme ? "hsl(0, 0%, 0%)" : "hsl(0, 0%, 100%)"}
           animate={isOpen ? "open" : "closed"}
-          initial={false}
+          initial="closed"
           variants={{
             closed: { opacity: 1 },
             open: { opacity: 0 },
@@ -54,15 +56,15 @@ export function MenuToggle({ toggle, isOpen, isLightTheme }) {
         />
         <Path
           animate={isOpen ? "open" : "closed"}
-          initial={false}
+          initial="closed"
           variants={{
             closed: {
               d: "M 2 16.346 L 20 16.346",
-              stroke: "hsl(100%, 100%, 100%)",
+              stroke: isLightTheme ? "hsl(0, 0%, 0%)" : "hsl(100%, 100%, 100%)",
             },
             open: {
               d: "M 3 2.5 L 17 16.346",
-              stroke: "hsl(100%, 100%, 100%)",
+              stroke: isLightTheme ? "hsl(0, 0%, 0%)" : "hsl(100%, 100%, 100%)",
             },
           }}
           transition={transition}
