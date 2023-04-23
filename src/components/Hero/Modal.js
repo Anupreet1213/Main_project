dd9db7c4a0e76a71f6ff19ea9bcbb4f3de071d35
Anupreet1213 <anupreet.2003@gmail.com>
2023-04-23 20:58:00 +0530

    Added responsive in modal

--- src/components/Hero/Modal.js
+++ src/components/Hero/Modal.js
@@ -11,29 +11,49 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "#2b2b2b",
-  border: "2px solid #000",
+  // border: "2px solid #000",
   boxShadow: 24,
   color: "white",
   p: 4,
   display: "flex",
-  flexDirection :"column",
-  alignItems: "center"
+  flexDirection: "column",
+  alignItems: "center",
+  outline: "none",
+  borderRadius: "12px",
 };
 
 const style2 = {
-    marginBottom : "6%"
-}
+  marginBottom: "6%",
+  display: "flex",
+  alignItems: "center",
+  width: "20vw",
+  justifyContent: "space-between",
+
+  "@media (max-width: 568px)": {
+    width: "60vw",
+  },
+  "@media (min-width: 569px) and (max-width: 1050px)": {
+    width: "40vw",
+  },
+};
 
 const boxStyle = {
-    width: "20vw",
-    padding: "6%",
-    backgroundColor: "#383838",
-    marginBottom: "2%",
-    borderRadius: "15px",
-    fontWeight: "600",
-    display: "flex",
-    justifyContent: "space-between"
-}
+  width: "20vw",
+  padding: "6%",
+  backgroundColor: "#383838",
+  marginBottom: "2%",
+  borderRadius: "15px",
+  fontWeight: "600",
+  display: "flex",
+  justifyContent: "space-between",
+
+  "@media (max-width: 568px)": {
+    width: "60vw",
+  },
+  "@media (min-width: 569px) and (max-width: 968px)": {
+    width: "40vw",
+  },
+};
 
 const ModalSection = ({ handleOpen, open, setOpen }) => {
   const handleClose = () => setOpen(false);
@@ -47,19 +67,29 @@ const ModalSection = ({ handleOpen, open, setOpen }) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography sx={style2} id="modal-modal-title" variant="h6" component="h2">
-            Connect Wallet
+          <Typography
+            sx={style2}
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+          >
+            <Typography>Connect Wallet</Typography>
+            <i
+              class="fa fa-close"
+              style={{ cursor: "pointer" }}
+              onClick={handleClose}
+            ></i>
           </Typography>
-          <Box sx={boxStyle} >
-            <Typography >MetaMask</Typography>
+          <Box sx={boxStyle}>
+            <Typography>MetaMask</Typography>
             <i class="fa-solid fa-cloud"></i>
           </Box>
-          <Box sx={boxStyle} >
-            <Typography >MetaMask</Typography>
+          <Box sx={boxStyle}>
+            <Typography>MetaMask</Typography>
             <i class="fa-solid fa-cloud"></i>
           </Box>
-          <Box sx={boxStyle} >
-            <Typography >MetaMask</Typography>
+          <Box sx={boxStyle}>
+            <Typography>MetaMask</Typography>
             <i class="fa-solid fa-cloud"></i>
           </Box>
         </Box>
